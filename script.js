8071ed097a234497e939512ca7d01033118ced48
Kevin Phan <57550661+kphanplan@users.noreply.github.com>
2020-09-18 03:03:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-//Game settings
-var playing = false;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var mainColor = "#32CD32";
 var score = 0;
 var level = 1;
 var lives = 3;
-alert("HIT OK TO START GAME");
-//GAME PROPERTIES
+alert(
+  "WELCOME TO BRICK BLASTER! \n HIT OK TO START THE GAME \n ↓ DO NOT CHECK THE BOX ↓"
+);
+
+//---GAME---//
 function game() {
   //this clears and creates "fps"
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -17,6 +18,12 @@ function game() {
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
+  //reset missle on collision
+  if (mY < 0) {
+    mY = canvas.height - 30;
+    mVel = 0;
+    projectile = false;
+  }
   //ends the game if ball hits bottom Y wall
   if (y + dy < ballRadius) {
     dy = -dy;
@@ -25,9 +32,13 @@ function game() {
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
       paddleBonkSound();
+      if (missles < 3) {
+        missles++;
+      }
     } else {
       //prompts game over
       lives--;
+      lifeLost();
       if (!lives) {
         alert("GAME OVER");
         document.location.reload();
@@ -54,20 +65,25 @@ function game() {
   //updates the ball's position
   x += dx;
   y += dy;
+  mY += mVel;
 }
-
+//refresh rate
+var interval = setInterval(game, 10);
 function generateLevel() {
   drawScore();
   drawLives();
   drawLevel();
+  drawMCount();
+
   drawBall();
   drawPaddle();
+  drawMissle();
+
   collisionDetection();
   drawBricks();
 }
 
 //generates the ball every 10ms
-var interval = setInterval(game, 10);
 
 //AUDIO
 function bonkSound() {
@@ -89,6 +105,12 @@ function pewSound() {
   sound.play();
 }
 
+function lifeLost() {
+  var sound = document.getElementById("lifeLost");
+  sound.volume = 0.5;
+  sound.play();
+}
+
 function bgm() {
   var sound = document.getElementById("bgm");
   sound.volume = 0.2;
@@ -100,7 +122,13 @@ function victorySound() {
   sound.play();
 }
 
-//PLAYER STATS
+var vid = document.getElementById("muteicon");
+vid.addEventListener("click", function() {
+  bgm.muted = true|false;
+})
+
+
+//UI STATS
 function drawScore() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD";
@@ -119,13 +147,13 @@ function drawLives() {
   ctx.fillText("LIVES: " + lives, canvas.width - 75, 20);
 }
 
-//Player controls
-document.addEventListener("click", function () {
-  pewSound();
-  score = winThreshold - 1;
-});
+function drawMCount() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("MISSLES: " + missles, 8, 460);
+}
 
-//keyboard controls
+//---CONTROLS---//
 var rightPressed = false;
 var leftPressed = false;
 document.addEventListener("keydown", keyDownHandler, false);
@@ -147,12 +175,22 @@ function keyUpHandler(e) {
   }
 }
 
-//paddle variables
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    if (missles > 0 && projectile == false) {
+      pewSound();
+      mVel = -5;
+      missles--;
+      projectile = true;
+    }
+  }
+};
+
+//---PADDLE---//
 var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleWidth = 90;
 var paddleX = (canvas.width - paddleWidth) / 2;
 
-//generates the Paddle
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -161,16 +199,17 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-//ball start position
+//---BALL---//
+
+//position
 var x = canvas.width / 2;
 var y = canvas.height - 30;
-//ball velocity
-var dx = 2;
-var dy = -2;
+
+var dx = 2.0;
+var dy = -2.0;
 //ball hitbox
 var ballRadius = 10;
 ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-
 //generates the ball
 function drawBall() {
   ctx.beginPath();
@@ -180,7 +219,22 @@ function drawBall() {
   ctx.closePath();
 }
 
-//Brick variables
+//---MISSLE---//
+var missles = 0;
+var mVel = 0;
+var mY = canvas.height - 30;
+var fireX;
+var projectile = false;
+function drawMissle() {
+  ctx.beginPath();
+  fireX = paddleX;
+  ctx.rect(fireX + 37.5, mY, 15, 50);
+  ctx.fillStyle = "orange";
+  ctx.fill();
+  ctx.closePath();
+}
+
+//---BRICKS---//
 var brickRowCount = 4;
 var brickColumnCount = 5;
 var brickWidth = 120;
@@ -189,7 +243,6 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 40;
 
-//generates Z number of bricks with status of 1\
 var bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
@@ -207,7 +260,6 @@ function brickReset() {
   }
 }
 
-//draw the bricks
 function drawBricks() {
   for (var c = 0; c < brickColumnCount; c++) {
     for (var r = 0; r < brickRowCount; r++) {
@@ -227,7 +279,6 @@ function drawBricks() {
 }
 
 var winThreshold = brickRowCount * brickColumnCount;
-// create brick hitboxes
 function collisionDetection() {
   for (var c = 0; c < brickColumnCount; c++) {
     for (var r = 0; r < brickRowCount; r++) {
@@ -243,27 +294,40 @@ function collisionDetection() {
           b.status = 0;
           score++;
           bonkSound();
-          console.log(bricks);
-          console.log(b);
-          //ends the game when you have no bricks left
-          if (score == winThreshold) {
-            victorySound();
-            alert("LEVEL COMPLETE NEXT LEVEL?");
-            if (level < 3) {
-              brickRowCount++;
-            }
-            if (level < 5) {
-              dx += 0.3;
-              dy += 0.3;
-            }
-            if (level >= 3 && level < 5) {
-              brickWidth -= 17.5;
-              brickPadding -= 1;
-              brickColumnCount++;
-            }
-            level++;
-            brickReset();
-            winThreshold += brickRowCount * brickColumnCount;
+        }
+        //missle collision (fireX and mY are missile position)
+        if (
+          fireX > b.x &&
+          fireX < b.x + brickWidth &&
+          mY > b.y &&
+          mY < b.y + brickHeight
+        ) {
+          mY = canvas.height - 30;
+          mVel = 0;
+          projectile = false;
+          b.status = 0;
+          score++;
+          bonkSound();
+        }
+        if (score == winThreshold) {
+          victorySound();
+          alert("LEVEL COMPLETE NEXT LEVEL?");
+          if (level < 3) {
+            brickRowCount++;
+          }
+          if (level >= 3 && level < 5) {
+            brickWidth -= 17.5;
+            brickPadding -= 1;
+            brickColumnCount++;
+          }
+          level++;
+          brickReset();
+          winThreshold += brickRowCount * brickColumnCount;
+          x = canvas.width / 2;
+          y = canvas.height - 30;
+          //flips the ball direction so it doesn't instant-kill you
+          if (dy > 0) {
+            dy = -dy;
           }
         }
       }
